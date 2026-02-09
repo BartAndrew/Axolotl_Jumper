e3b48eb77246fc9a06887f098cacf4a872199be6
BartAndrew <Bart.Andrew@gmail.com>
2026-02-09 20:15:52 +1100

    commit

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'axolotl-jumper-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  'index.html',
+  'manifest.json',
+  'service-worker.js',
+  'axolotl.png',
+  'platforms.png',
+  'background.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
